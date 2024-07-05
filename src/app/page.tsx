c266f850b7d5c415f8ab89a4846c5f49c0c1fd28
zaheerbasha <syed.basha@razorpay.com>
2024-07-05 06:56:29 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,13 @@ import { useSession } from 'next-auth/react'
 
 export default async function Home() {
 
- 
-
-
-  const res = await fetch('http://localhost:8007/dashboard',{
+  const res = await fetch('https://renegades-stage.dev.razorpay.in/dashboard',{
     headers: {
       "Content-Type":"application/json",
       "Authorization": "Basic cm5yOnJucg=="
     }
   })
   const resp = await res.json()
-
-
 
   return (
     <Dashboard data={resp}/>
--- src/utils/helperBackendFunctions.ts
+++ src/utils/helperBackendFunctions.ts
@@ -9,6 +9,28 @@ export async function getUserEmail() {
   return session?.user?.email;
 }
 
+export async function getEmployeeInfo() {
+  const session = await getServerSession();
+  if (!session) {
+    return null;
+  }
+  const email = session?.user?.email;
+  console.log(email)
+  const empUrl = 'https://renegades-stage.dev.razorpay.in/employee/'+email
+  console.log(empUrl)
+  const res = await fetch(empUrl,{
+    headers: {
+      "Content-Type":"application/json",
+      "Authorization": "Basic cm5yOnJucg=="
+    }
+  })
+  const resp = await res.json()
+  if (!resp) {
+    return null;
+  }
+  return resp
+}
+
 export async function getUserName() {
   const session = await getServerSession();
   if (!session) {
